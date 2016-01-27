2f2be7f78acf88ea5471e37e88d219ec834a8a53
Nishant Gupta <nish.gupta01@gmail.com>
2016-01-27 15:36:53 +0530

    Moved things around, add main.js to separate js from html.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/web'));
+
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile('web/index.html');
 });
 
 io.on('connection', function(socket){
