c15a925bb8ce90d40575c1d122eba47802ca7436
Nishant Gupta <nish.gupta01@gmail.com>
2016-01-28 17:44:59 +0530

    Some Style

--- web/main.js
+++ web/main.js
@@ -3,13 +3,38 @@ $(function() {
     var socket = io();
     socket.emit('add user', username);
 
+    var colors = [
+        '#00BFFF', '#228B22', '#4B0082', '#800000',
+        '#FF4500', '#7B68EE', '#D2691E', '#FFA500'
+    ]
+
 	$('form').submit(function(){
 		socket.emit('chat message', $('#m').val());
 		$('#m').val('');
 		return false;
 	});
     
+    function getUserColor(username) {
+        var sum = 0;
+        for (var i = 0; i < username.length; i++) {
+            sum += username.charCodeAt(i);
+        }
+        sum %= colors.length;
+        return colors[sum];
+    }
+
 	socket.on('chat message', function(data){
-		$('#messages').append($('<li>').text(data.username + " : " + data.message));
+        var $usernameDiv = $('<span class="username"/>')
+            .text(data.username + ":")
+            .css('color', getUserColor(data.username));
+        
+        var $msgTextDiv = $('<span class="msgText">')
+            .text(data.message);
+
+        var $messageDiv = $('<li class="message"/>')
+            .data('username', data.username)
+            .append($usernameDiv, $msgTextDiv);
+
+        $('#messages').append($messageDiv);
 	});
 });
